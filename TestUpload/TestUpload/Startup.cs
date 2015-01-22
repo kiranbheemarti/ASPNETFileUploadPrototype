b86226f8e82bc1538aeac1a1048da26adcc6aa71
Kiran Kumar Bheemarti <kiran@bheemarthis.com>
2015-01-22 15:30:04 -0500

    Initial check in....

---
+++ TestUpload/TestUpload/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestUpload.Startup))]
+namespace TestUpload
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
